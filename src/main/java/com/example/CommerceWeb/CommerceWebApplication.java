8c80734cf7e5caaf6e88c8ef458f61ca2955b9d6
Oussoumanou Benoit <OusmanBenoit@gmail.com>
2024-01-11 10:38:49 +0100

    initialisation

---
+++ src/main/java/com/example/CommerceWeb/CommerceWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.CommerceWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommerceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommerceWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CommerceWeb/CommerceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CommerceWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommerceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
